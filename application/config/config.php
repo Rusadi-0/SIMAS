d447d6eef2458b86bfaf1b647218355d74b3271c
Hadi Rusadi <77903323+Rusadi-0@users.noreply.github.com>
2022-09-06 06:33:47 +0800

    bug sessieon
    perbaiki bug session dari satu tahu menjadi 7200 detik

--- application/config/config.php
+++ application/config/config.php
@@ -379,8 +379,8 @@ $config['encryption_key'] = '';
 */
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 60*60*24*30*12; //1 tahun
-$config['sess_save_path'] = null;
+// $config['sess_expiration'] = 60*60*24*30*12; //1 tahun
+$config['sess_expiration'] = 7200;
 $config['sess_match_ip'] = false;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = false;
